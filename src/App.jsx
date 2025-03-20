a1257c0f2e19e560d55cbf436b91a1cdbc47bfa2
Nicolas <nicolasdelfino585@gmail.com>
2025-03-19 22:02:03 -0300

    proyecto casi terminado

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import Guitar from "./components/Guitar";
+import Header from "./components/Header";
+import { db } from "./data/db";
+
+function App() {
+  const [data, setData] = useState(db);
+  const [cart, setCart] = useState([]);
+
+  function addToCart(item) {
+    const itemExist = cart.findIndex((guitar) => guitar.id === item.id);
+    if (itemExist >= 0) {
+      const updatedCart = [...cart];
+      updatedCart[itemExist].quantity++;
+      setCart(updatedCart);
+    } else {
+      item.quantity = 1;
+      setCart([...cart, item]);
+    }
+  }
+
+  function removeFromCart(id) {
+    setCart((prevCart) => prevCart.filter((guitar) => guitar.id !== id));
+  }
+
+  return (
+    <>
+      <Header cart={cart} removeFromCart={removeFromCart} />
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+          {data.map((guitar) => (
+            <Guitar
+              key={guitar.id}
+              guitar={guitar}
+              setCart={setCart}
+              addToCart={addToCart}
+            />
+          ))}
+        </div>
+      </main>
+
+      <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+          <p className="text-white text-center fs-4 mt-4 m-md-0">
+            GuitarLA - Todos los derechos Reservados
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/data/db.js
@@ -0,0 +1,98 @@
+export const db = [
+  {
+    id: 1,
+    name: "Lukather",
+    image: "guitarra_01",
+    description:
+      "The Lukather guitar offers incredible versatility and precision, perfect for rock and blues enthusiasts. Its crafted tone and smooth playability are ideal for all levels.",
+    price: 299,
+  },
+  {
+    id: 2,
+    name: "SRV",
+    image: "guitarra_02",
+    description:
+      "Inspired by the legendary Stevie Ray Vaughan, the SRV guitar captures his signature blues sound. Its powerful, warm tone is perfect for soulful solos and intense riffs.",
+    price: 349,
+  },
+  {
+    id: 3,
+    name: "Borland",
+    image: "guitarra_03",
+    description:
+      "The Borland guitar delivers a sharp, crisp sound, designed for metal and hard rock players. Its durable build and aggressive tone will satisfy the most demanding musicians.",
+    price: 329,
+  },
+  {
+    id: 4,
+    name: "VAI",
+    image: "guitarra_04",
+    description:
+      "The Vai guitar, inspired by Steve Vai’s iconic style, offers superb precision and effortless bending. A true choice for shredders looking for exceptional performance.",
+    price: 299,
+  },
+  {
+    id: 5,
+    name: "Thompson",
+    image: "guitarra_05",
+    description:
+      "Thompson guitars are crafted for smooth jazz and blues sounds. With a rich, warm tone, it’s an excellent choice for those who enjoy dynamic playing with clarity.",
+    price: 399,
+  },
+  {
+    id: 6,
+    name: "White",
+    image: "guitarra_06",
+    description:
+      "White guitars stand out with their clean design and bright tone. Ideal for indie and alternative music, they offer a smooth, crisp sound that fits any style.",
+    price: 329,
+  },
+  {
+    id: 7,
+    name: "Cobain",
+    image: "guitarra_07",
+    description:
+      "Inspired by Kurt Cobain, this guitar is perfect for grunge and punk rock. Its raw tone and simple yet powerful design deliver the true spirit of Cobain’s sound.",
+    price: 349,
+  },
+  {
+    id: 8,
+    name: "Dale",
+    image: "guitarra_08",
+    description:
+      "The Dale guitar is a high-performance instrument designed for heavy metal and rock musicians. With a strong, aggressive tone, it’s built to withstand the hardest of playing styles.",
+    price: 379,
+  },
+  {
+    id: 9,
+    name: "Krieger",
+    image: "guitarra_09",
+    description:
+      "The Krieger guitar offers a unique combination of vintage and modern tones. Its versatile design allows for smooth leads and powerful rhythm playing, perfect for any genre.",
+    price: 289,
+  },
+  {
+    id: 10,
+    name: "Campbell",
+    image: "guitarra_10",
+    description:
+      "Campbell guitars provide excellent sustain and clarity. A fantastic choice for rock and metal, it delivers high output with a tight, defined tone that cuts through the mix.",
+    price: 349,
+  },
+  {
+    id: 11,
+    name: "Reed",
+    image: "guitarra_11",
+    description:
+      "Reed guitars combine classic design with modern playability. Known for their exceptional comfort and balanced tone, they’re great for versatile musicians seeking a smooth experience.",
+    price: 399,
+  },
+  {
+    id: 12,
+    name: "Hazel",
+    image: "guitarra_12",
+    description:
+      "Hazel guitars are known for their rich, warm tone and excellent craftsmanship. A great choice for jazz and blues players, offering clarity, resonance, and unmatched comfort.",
+    price: 379,
+  },
+];
